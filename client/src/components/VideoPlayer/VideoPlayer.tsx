2c21bec16df1ae6ab0415c52f244ecad9c556a68
Derrick Hawkins <dhawkvq@yahoo.com>
2020-08-21 18:03:26 -0700

    OOF. should have checked into video control before trying to re-invent the wheel.

--- client/src/components/VideoPlayer/VideoPlayer.tsx
+++ client/src/components/VideoPlayer/VideoPlayer.tsx
@@ -1,15 +1,12 @@
 import React, { useRef, useState, useEffect } from 'react'
 import './videoPlayer.scss'
-import { PlayButton, PauseButton } from './components'
 import { grabVidById } from '../../utility'
+
 
 const VideoPlayer = ({ match }) => {
 
-
   const [videoUrl, setVideoUrl] = useState(null)
   const [videoInfo, setVideoInfo] = useState(null)
-  const [vidPlaying, setVidPlaying] = useState(false)
-  const [showControls, setShowControls] = useState(false)
   const [vidError, setVidError] = useState(null)
   const vidRef = useRef(null);
 
@@ -25,53 +22,18 @@ const VideoPlayer = ({ match }) => {
       .catch(error => setVidError(error.message))
   }, [match.params])
 
-  
-  useEffect(() => {
-    if(vidRef.current && videoUrl){
-      setVidPlaying((playState: boolean) => !playState)
-      vidRef.current.play()     
-    }
-  }, [vidRef,videoUrl,setVidPlaying])
-
-  const handlePlay = () => {
-    vidRef.current.play()
-    setVidPlaying(true)
-  }
-
-  const handlePause = () => {
-    vidRef.current.pause()
-    setVidPlaying(false)
-  }
-
-
   return (
     <div className='container'>
-      <div 
-        className='vidWrapper' 
-        onMouseEnter={() => setShowControls(true)}
-        onMouseLeave={() => setShowControls(false)}
-      >
-        { videoUrl &&
-        <>
-          {
-            showControls &&
-            <div style={{ border: '1px dashed white' }}
-              className='controls'
-            > 
-              {
-                vidPlaying ?
-                <PauseButton handleClick={handlePause} />
-                :
-                <PlayButton handleClick={handlePlay}/>
-              }
-            </div>
-          }
-          <video ref={vidRef}>
-            <source src={videoUrl}/>
-          </video>
-        </>
-        }
-      </div>
+      { videoUrl &&
+        <video ref={vidRef} 
+          autoPlay
+          controls 
+          controlsList='nodownload' 
+          disablePictureInPicture
+        >
+          <source src={videoUrl}/>
+        </video>
+      }
     </div>
   )
 }
